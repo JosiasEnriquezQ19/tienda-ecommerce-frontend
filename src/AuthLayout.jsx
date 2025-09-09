93e979fabc2779dba088c97b2c3a8804a7725c1c
Josias <enriquezquispejosiasjosh@gmail.com>
2025-09-09 17:07:24 -0500

    Optimiza el formulario de registro para vista móvil

--- src/AuthLayout.jsx
+++ src/AuthLayout.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import SimpleHeader from './components/SimpleHeader';
 import './styles.css';
+import './auth-layout.css';
 
 export default function AuthLayout() {
   return (
     <div className="auth-layout">
       <SimpleHeader />
       <main>
-        <Outlet />
+        <div className="auth-centered-container">
+          <Outlet />
+        </div>
       </main>
     </div>
   );
